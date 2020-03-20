1b3394b9350ba3c78a7dbe23d61a40f4dcfd3d4e
rizaldieqy <rizalasshidieqy09@gmail.com>
2020-03-20 12:49:01 +0700

    lagi.php edited

--- lagi.php
+++ lagi.php
@@ -167,7 +167,7 @@
                         Sabtu Jam 10.00-12.0
                         </label>
                     </div><br><br><hr>
-                    <button type="button" class="btn btn-primary form2">Kirim</button>
+                    <button type="button" class="btn btn-primary form2" OnClick="tekan()">Kirim</button>
                 </form>
             </div>
         </div>
@@ -179,5 +179,11 @@
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script>
+    function tekan(){
+        alert("Pendaftaran Berhasil");
+        window.location.href="isicalonsiswa.php"
+    }
+</script>
 </body>
 </html>
